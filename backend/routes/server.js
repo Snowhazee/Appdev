0a55e815485e374b1d14be37dbbec38a8fcdb5eb
Snowhazee1 <eaekeak@gmail.com>
2026-02-21 05:17:25 +0700

    Initial back end

---
+++ backend/routes/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+console.log("JWT_SECRET is:", process.env.JWT_SECRET);
+const express = require("express");
+
+const accountRoutes = require("./auth");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.use("/", accountRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+//# sourceURL=backend/routes/server.js
