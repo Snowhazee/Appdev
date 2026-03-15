8a87aeecd8eaf14048227d49dae30e39711ef231
Snowhazee <62649306+Snowhazee@users.noreply.github.com>
2026-03-16 00:59:18 +0700

    add mongoose and front end structure

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -6,3 +6,13 @@ const router = express.Router(); // Router is a mini server
 router.get('/products', shopController.getProducts); // /products is the endpoint, shopController.getProducts is the controller function that will be called when the endpoint is hit
 router.get('/products/:id', shopController.getProductByIndex); // :id is a route parameter, it will be replaced by the actual id of the product when the endpoint is hit
 
+router.get('/products', (req, res) => {
+  const products = [
+    { id: 1, name: 'Laptop' },
+    { id: 2, name: 'Mouse' },
+    { id: 3, name: 'Keyboard' }
+  ]
+
+  res.json(products)
+})
+module.exports = router;
